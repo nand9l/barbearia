cae58ee09fbcf46e6e5637d4c56ff8af3cfc6d5a
José Fernando <166651961+nand9l@users.noreply.github.com>
2025-04-27 16:22:33 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,14 +2,24 @@
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+    var displayYear = document.querySelector("#displayYear");
+    if (displayYear) {
+        displayYear.innerHTML = currentYear;
+    }
 }
 
 getYear();
 
-
 // isotope js
 $(window).on('load', function () {
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: false,
+        masonry: {
+            columnWidth: ".all"
+        }
+    });
+
     $('.filters_menu li').click(function () {
         $('.filters_menu li').removeClass('active');
         $(this).addClass('active');
@@ -17,22 +27,16 @@ $(window).on('load', function () {
         var data = $(this).attr('data-filter');
         $grid.isotope({
             filter: data
-        })
+        });
     });
-
-    var $grid = $(".grid").isotope({
-        itemSelector: ".all",
-        percentPosition: false,
-        masonry: {
-            columnWidth: ".all"
-        }
-    })
 });
 
 // nice select
-$(document).ready(function() {
-    $('select').niceSelect();
-  });
+$(document).ready(function () {
+    if ($('select').length) {
+        $('select').niceSelect();
+    }
+});
 
 /** google_map js **/
 function myMap() {
@@ -40,31 +44,37 @@ function myMap() {
         center: new google.maps.LatLng(40.712775, -74.005973),
         zoom: 18,
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var mapElement = document.getElementById("googleMap");
+    if (mapElement) {
+        var map = new google.maps.Map(mapElement, mapProp);
+    }
 }
 
 // client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 0,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        768: {
-            items: 2
-        },
-        1000: {
-            items: 2
-        }
+$(document).ready(function () {
+    if ($(".client_owl-carousel").length) {
+        $(".client_owl-carousel").owlCarousel({
+            loop: true,
+            margin: 0,
+            dots: false,
+            nav: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            navText: [
+                '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+                '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+            ],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                1000: {
+                    items: 2
+                }
+            }
+        });
     }
 });
